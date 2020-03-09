c9876540c5c23576f996ac9da20f4c57108d9cb8
razvanmierla <razvan.mierla13@gmail.com>
2020-03-09 02:26:40 +0200

    Added put request + suitable message for not enough cash money

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, OnChanges } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { HttpClient } from "@angular/common/http";
-import { Product } from "./product";
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Product } from './product';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "Vending Machine";
+  title = 'Vending Machine';
   products: Array<Product>;
   usesCash = false;
   productId: number;
@@ -17,10 +17,19 @@ export class AppComponent implements OnInit {
   sumPaid: number;
   rest: number;
   selectedProduct: Product;
+  notEnoughMoney = false;
+  tooMuchMoney = false;
 
   private async getProducts(): Promise<Product[]> {
     return await this.http
-      .get<Product[]>(environment.productApiUrl, { responseType: "json" })
+      .get<Product[]>(environment.productApiUrl, { responseType: 'json' })
+      .toPromise();
+  }
+
+  private async updateProduct(productID: number,productQuantity: number): Promise<Product> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return await this.http
+      .put<Product>(environment.productApiUrl + '/' + productID + '/' + productQuantity, { responseType: 'json', headers})
       .toPromise();
   }
 
@@ -56,18 +65,25 @@ export class AppComponent implements OnInit {
 
   checkQuantity() {
     if (this.productQuantity > this.selectedProduct.quantity) {
-      console.log("Cantitate indisponibila. Stocul este de: " + this.selectedProduct.quantity);
+      console.log('Cantitate indisponibila. Stocul este de: ' + this.selectedProduct.quantity);
     }
   }
 
   calculateRest() {
-    var totalSumForProduct = this.productQuantity * this.selectedProduct.price;
+    let totalSumForProduct = this.productQuantity * this.selectedProduct.price;
     if (this.sumPaid != 0 && this.sumPaid > totalSumForProduct) {
-      console.log(this.sumPaid, totalSumForProduct)
+      console.log(this.sumPaid, totalSumForProduct);
       this.rest = this.sumPaid - totalSumForProduct;
+      this.tooMuchMoney = true;
+      this.notEnoughMoney = false;
+    } else {
+      this.tooMuchMoney = false;
+      this.notEnoughMoney = true;
     }
   }
-  submit() { }
+  submit() {
+    this.updateProduct(this.productId,this.productQuantity);
+   }
 
   constructor(private http: HttpClient) { }
 }
